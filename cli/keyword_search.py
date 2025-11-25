764c4735cabf246a0a370f45ed858d6a146eff9b
Jeffee Jenson <jeffee125@gmail.com>
2025-11-25 14:44:13 +0530

    added Stemming functionality

--- cli/keyword_search.py
+++ cli/keyword_search.py
@@ -1,32 +1,20 @@
-from search_util import LIMIT, load_movies
+from search_util import LIMIT, load_movies , load_stopwords
 import string
+from nltk.stem import PorterStemmer
+
+stemmer = PorterStemmer()
 
 def search_command(args : str ,limit: int = LIMIT) -> list[dict]:
     movies = load_movies()
     results = []
     for movie in movies:
-        query_tokens = tokeniser(args)
-        title_tokens = tokeniser(movie["title"])
-        """
-        for args in args_query:
-            if arg in title and len(results) < limit:
-                if movie in results:
-                    continue
-                else:
-                    results.append(movie) 
-        #
-                    if movie not in results else results.append("")
-        """
+        query_tokens = stem_tokens(args)
+        title_tokens = stem_tokens(movie["title"])
         if matching_tokens(query_tokens,title_tokens):
             results.append(movie) 
             if(len(results) > limit):
                 break
-
-
-        
-
     results.sort(key = lambda movie : movie["id"])
-            
     return results
 
 def pre_process( text : str ) -> str:
@@ -38,10 +26,30 @@ def tokeniser ( text : str) -> list[str]:
     text = pre_process(text)
     return text.split()
 
+def stop_words(text : str) -> list[str]:
+    reduced_tokens = tokeniser(text)
+    stop_words_list = load_stopwords()
+    final_tokens = []
+    for token in reduced_tokens:
+        if token not in stop_words_list:
+            final_tokens.append(token)
+    return final_tokens
+
+def stem_tokens(text : str) -> list[str]:
+    unstemmed_tokens = stop_words(text)
+    stemmed_tokens = []
+    for unstemmed_token in  unstemmed_tokens:
+        stemmed_tokens.append(stemmer.stem(unstemmed_token))
+    return stemmed_tokens
+    
+
+    
+
 def matching_tokens(str1 : list[str] , str2 : list[str]) -> bool:
     for s1 in str1:
-        if s1 in str2:
-            return True
+        for s2 in str2:
+            if s1 in s2:
+                return True
     return False
 
 
--- cli/search_util.py
+++ cli/search_util.py
@@ -3,12 +3,20 @@ import os
 
 LIMIT = 5
 PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
-DATA_PATH = os.path.join(PROJECT_ROOT , "data" , "movies.json")
+DATA_PATH_MOVIE = os.path.join(PROJECT_ROOT , "data" , "movies.json")
+DATA_PATH_STOP_WORDS = os.path.join(PROJECT_ROOT , "data" , "stopwords.txt")
 
 
 
 def load_movies() -> list[dict]:
-    with open(DATA_PATH, 'r') as file:
+    with open(DATA_PATH_MOVIE, 'r') as file:
         data = json.load(file)
         movies_list = data["movies"]
         return movies_list
+
+def load_stopwords() -> list[str]:
+    with open(DATA_PATH_STOP_WORDS,'r') as file:
+        stop_words = file.read()
+        stop_words_list = stop_words.splitlines()
+    return stop_words_list
+        
