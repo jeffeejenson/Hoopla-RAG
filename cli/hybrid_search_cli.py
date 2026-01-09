2fa5f3e65ce52632bd567051a6a040f1e193445b
Jeffee Jenson <jeffee125@gmail.com>
2026-01-09 23:34:35 +0530

    added query re-writing

--- cli/hybrid_search_cli.py
+++ cli/hybrid_search_cli.py
@@ -1,6 +1,10 @@
 import argparse
+import os
 from hybrid_search import normalise_scores,HybridSearch
 from search_util import load_movies
+from dotenv import load_dotenv
+from google import genai
+
 
 
 def main() -> None:
@@ -19,6 +23,8 @@ def main() -> None:
     rrf_search_parser.add_argument("query" , type = str , help = "Type in the qquery")
     rrf_search_parser.add_argument("-k" , type = int ,default= 60, help = "K value for rrf")
     rrf_search_parser.add_argument("--limit" , type = int ,default= 5, help = "limit of movies")
+    rrf_search_parser.add_argument("--enhance",type=str,choices=["spell","rewrite","expand"],help="Query enhancement method",)
+    rrf_search_parser.add_argument("--rerank-method" , type = str ,choices =["individual"], help = "rerank method")
 
 
 
@@ -47,15 +53,98 @@ def main() -> None:
                 print(f"{result["Description"]} ")
         
         case "rrf-search":
+
             hyb = HybridSearch(load_movies())
+            results = []
+            if args.enhance == None:
+                results = hyb.rrf_search(args.query, args.k, args.limit)
+            elif args.enhance == "spell":
+                load_dotenv()
+                api_key = os.environ.get("GEMINI_API_KEY")
+                client = genai.Client(api_key=api_key)
+                prompt = f"""Fix any spelling errors in this movie search query.
 
-            results = hyb.rrf_search(args.query, args.k, args.limit)
+                            Only correct obvious typos. Don't change correctly spelled words.
 
+                            Query: "{args.query}"
+
+                            If no errors, return the original query.
+                            Corrected:"""
+                response = client.models.generate_content( model='gemini-2.5-flash-lite', contents=prompt)
+                query = response.text
+                results = hyb.rrf_search(query, args.k, args.limit)
+                #'gemini-2.5-flash-lite'
+                #'gemini-2.0-flash-001'
+                print(f"Enhanced query ({args.enhance}): '{args.query}' -> '{query}'\n")
+            
+            
+            elif args.enhance == "rewrite":
+                load_dotenv()
+                api_key = os.environ.get("GEMINI_API_KEY")
+                client = genai.Client(api_key=api_key)
+                prompt = f"""Rewrite this movie search query to be more specific and searchable.
+
+                            Original: "{args.query}"
+
+                            Consider:
+                                - Common movie knowledge (famous actors, popular films)
+                                - Genre conventions (horror = scary, animation = cartoon)
+                                - Keep it concise (under 10 words)
+                                - It should be a google style search query that's very specific
+                                - Don't use boolean logic
+
+                            Examples:
+
+                            - "that bear movie where leo gets attacked" -> "The Revenant Leonardo DiCaprio bear attack"
+                            - "movie about bear in london with marmalade" -> "Paddington London marmalade"
+                            -  "scary movie with bear from few years ago" -> "bear horror movie 2015-2020"
+
+                            Rewritten query:"""
+                response = client.models.generate_content( model='gemini-2.5-flash-lite', contents=prompt)
+                query = response.text
+                results = hyb.rrf_search(query, args.k, args.limit)
+                #'gemini-2.5-flash-lite'
+                #'gemini-2.0-flash-001'
+            
+                print(f"Enhanced query ({args.enhance}): '{args.query}' -> '{query}'\n")
+
+            elif args.enhance == "expand":
+                load_dotenv()
+                api_key = os.environ.get("GEMINI_API_KEY")
+                client = genai.Client(api_key=api_key)
+                prompt =f"""Expand this movie search query with related terms.
+
+                            Add synonyms and related concepts that might appear in movie descriptions.
+                            Keep expansions relevant and focused.
+                            This will be appended to the original query.
+
+                            Examples:
+
+                            - "scary bear movie" -> "scary horror grizzly bear movie terrifying film"
+                            - "action movie with bear" -> "action thriller bear chase fight adventure"
+                            - "comedy with bear" -> "comedy funny bear humor lighthearted"
+
+                            Query: "{args.query}"""
+                response = client.models.generate_content( model='gemini-2.5-flash-lite', contents=prompt)
+                query = response.text
+                results = hyb.rrf_search(query, args.k, args.limit)
+                #'gemini-2.5-flash-lite'
+                #'gemini-2.0-flash-001'
+            
+                print(f"Enhanced query ({args.enhance}): '{args.query}' -> '{query}'\n")
+            
+            if args.rerank_method == "individual":
+
+
+
+
+                            
             for i,result in enumerate(results,1):
                 print(f"{i}. {result["title"]} ")
                 print(f"RRF Score :{result["rrf_score"]:.4f}")
                 print(f"BM25 RANK : {result["bm25_rank"]:.4f}   ,Semantic Rank : {result["sem_rank"]:.4f}")
                 print(f"{result["description"][:100]} ")
+
 
 
 
